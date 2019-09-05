9f34aa0368f2626648a0884536b4d02d92ee71f4
Mahmud <mahmud.siraj@outlook.com>
2019-09-05 01:03:17 +0100

    thymeleaf tags added

--- src/main/java/com/mahmud/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/mahmud/recipeapp/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -14,9 +15,7 @@ public class RecipeServiceImpl implements RecipeService{
 
     private final RecipeRepository recipeRepository;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
-    }
+    public RecipeServiceImpl(RecipeRepository recipeRepository) { this.recipeRepository = recipeRepository; }
 
     @Override
     public Set<Recipe> getRecipes() {
@@ -24,5 +23,17 @@ public class RecipeServiceImpl implements RecipeService{
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(Long l){
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+
+        if(!recipeOptional.isPresent()) {
+            throw new RuntimeException("Recipe Not Found!");
+        }
+
+        return recipeOptional.get();
     }
 }
